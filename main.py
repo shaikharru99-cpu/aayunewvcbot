5982dd92125400383ef7265df76aa606af796235
shaikharru99-cpu <shaikharru99@gmail.com>
2026-07-16 18:59:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-# main.py
+# --- TELEGRAM API CONFIGURATION ---
+# Get these from https://my.telegram.org
+API_ID = 39800351
+API_HASH = "2a6fbe5d5c92adf1b49f9667be3598c3"
+
+# --- BOT CONFIGURATION ---
+# Get this from @BotFather
+BOT_TOKEN = "8978249219:AAFz5VrmMNE_yFHeuD6y_vbi9r0BlIh5_UE" 
+
+# --- DATABASE CONFIGURATION ---
+# MongoDB Connection String (e.g., from MongoDB Atlas)
+MONGO_URL = "mongodb+srv://ArruBhai:NVPavPk34HtQ6RW1@cluster0.hhlnxa9.mongodb.net/?appName=Cluster0"
+
+# Configure the database name (Used to store sessions and settings)
+DB_NAME = "telegram_bot_db223344" 
+
+# --- ASSETS ---
+WELCOME_IMAGE = "https://i.ibb.co/ch8W4QmS/ARLTools.png"
+
+# --- LOGGING ---
+# Channel ID to send logs to (Start with -100). Set to None to disable.
+LOG_CHANNEL = -1004337410413
+# --- PERMISSIONS & ADMINS ---
+# The absolute owner of the bot (Full Access - Cannot be removed by admins)
+OWNER_ID = 6360979950
+
+# List of Admin User IDs who can control basic bot features (Engagement, joining VC, adding bots)
+# Admins cannot remove bots, change global limits, or wipe targets.
+ADMIN_IDS = [6360979950]
+
 import asyncio
 import logging
 import os
